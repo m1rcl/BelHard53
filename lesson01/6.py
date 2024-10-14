0bd9f606a03af1e76d9a137dacbad6d60f9b0e03
Konstantin Kiselev <47434113+m1rcl@users.noreply.github.com>
2024-10-14 13:57:40 +0300

    Update 6.py

--- lesson01/6.py
+++ lesson01/6.py
@@ -3,7 +3,24 @@
 Даны 4 переменные - a1, a2, a3, a4.
 1 - вывести True если все они дробные числа
 2 - вывести True если одна из них строка
-3 - вывести True если  одна пара переменных является целочисленным типом. 
+3 - вывести True если  одна пара переменных является целочисленным типом.
     Пары могут образовать только следующие переменные - a1-a3, a2-a4, a3-a4"
 """
 
+
+a1 = 2.0
+a2 = 2
+a3 = "name"
+a4 = 27
+
+print(a1, a2, a3, a4)
+
+if all(type(item) is float for item in (a1, a2, a3, a4)):
+    print("Все переменные дробные - ", True)
+
+if any(type(item) is str for item in (a1, a2, a3, a4)):
+    print("Хотя бы одна из переменных строка - ", True)
+
+#if all(map(lambda item: True if type(item) is not int else False, ((a1, a3), (a2, a4), (a3, a4)))):
+if any(all(type(item) is int for item in pair) for pair in ((a1, a3), (a2, a4), (a3, a4))):
+    print("Хотя бы одна из пар переменных является целочисленным типом - ", True)
