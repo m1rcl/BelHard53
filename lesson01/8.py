d324a0605d4f7e22ce3a0b180898aa701729155e
Konstantin Kiselev <47434113+m1rcl@users.noreply.github.com>
2024-10-14 13:59:50 +0300

    Update 8.py

--- lesson01/8.py
+++ lesson01/8.py
@@ -1,8 +1,12 @@
 '''
 Дан список содержащий в себе различные типы данных, отфильтровать таким
-образом, чтобы 
+образом, чтобы
  - остались только строки.
  - остался только логический тип.
- 
+
 '''
 
+
+some_list = ['hello', -3, True, 3.14]
+filtered_list = list(filter(lambda item: type(item) is str or type(item) is bool, some_list))
+print(filtered_list)
