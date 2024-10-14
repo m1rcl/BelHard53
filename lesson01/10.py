8cf81000698cba9ba260f4013241fc6602c2df1a
Konstantin Kiselev <kiselev-kostya@tut.by>
2024-10-14 21:38:44 +0300

    update 10.py

--- lesson01/10.py
+++ lesson01/10.py
@@ -10,3 +10,21 @@ foo0042 -> foo0043
 foo9 -> foo10
 foo099 -> foo100
 '''
+
+
+def inc_num_in_string(some_str):
+    if some_str.isalpha():
+        output_string = some_str + "1"
+        return output_string
+
+    for item in range(len(some_str)):
+        if some_str[item].isdigit():
+            num_in_string = some_str[item:len(some_str)+1]
+            output_string = some_str[0:item] + \
+                str(int(num_in_string) + 1).zfill(len(num_in_string))
+            return output_string
+
+
+user_input = input("Введите строку: ")
+inc_num_in_string(user_input)
+print(inc_num_in_string(user_input))
