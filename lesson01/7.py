d6858a12fe720c18f83db820e035fbac7de0c585
Konstantin Kiselev <47434113+m1rcl@users.noreply.github.com>
2024-10-14 13:59:27 +0300

    Update 7.py

--- lesson01/7.py
+++ lesson01/7.py
@@ -1,12 +1,15 @@
-'''
-Дан список содержащий в себе различные типы данных, отфильтровать таким
-образом, чтобы
- - остались только строки.
- - остался только логический тип.
+"""
+Запросить любое число не менее 10.
+Вывести на экран сумму квадратов каждой цифры составляющей это число.
+Например: дано 236 => 2*2 + 3*3 + 6*6 = 49
 
-'''
+"""
 
 
-some_list = ['hello', -3, True, 3.14]
-filtered_list = list(filter(lambda item: type(item) is str or type(item) is bool, some_list))
-print(filtered_list)
+user_num = input("Введите любое число: ")
+sum_of_digit = 0
+
+for digit in user_num:
+    sum_of_digit += float(digit)**2
+
+print(sum_of_digit)
