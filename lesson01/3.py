c67b73fd4299967d975d68021ec45146a3576b10
Konstantin Kiselev <47434113+m1rcl@users.noreply.github.com>
2024-10-14 13:56:42 +0300

    Update 3.py

--- lesson01/3.py
+++ lesson01/3.py
@@ -1,6 +1,15 @@
 """
-Дан словарь наблюдения за температурой 
-{"day1":18, "day2":22, "day3":7, "day4":11, "day5":14}. 
+Дан словарь наблюдения за температурой
+{"day1":18, "day2":22, "day3":7, "day4":11, "day5":14}.
 Отсортировать словарь по температуре в порядке возрастания и обратно.
 
 """
+
+
+temperature_dict = {"day1":18, "day2":22, "day3":7, "day4":11, "day5":14}
+
+sorted_temperature_dict = dict(sorted(temperature_dict.items(), key=lambda item: item[1]))
+print(sorted_temperature_dict)
+
+reversed_temperature_dict = dict(sorted(temperature_dict.items(), key=lambda item: item[1], reverse=True))
+print(reversed_temperature_dict)
