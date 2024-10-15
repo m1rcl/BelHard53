d4afedad7c8c6feb1d9204cd4c1d791b438a7e1e
Konstantin Kiselev <kiselev-kostya@tut.by>
2024-10-15 19:46:01 +0300

    Update 1.py

--- lesson01/1.py
+++ lesson01/1.py
@@ -6,7 +6,7 @@
 """
 
 
-def printn(counter, user_input):
+def printn(user_input):
     if user_input:
         print(f"строка {counter} : {user_input}")
 
@@ -17,4 +17,4 @@ while user_input:
     user_input = input(
         f"Введите строку {counter} для отображения в консоли, либо оставьте пустой для выхода: ")
     counter = counter + 1
-    printn(counter, user_input)
+    printn(user_input)
