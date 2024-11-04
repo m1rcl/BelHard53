960a51b7728d0f8692cb256c854c7744a2979dd3
Konstantin Kiselev <kiselev-kostya@tut.by>
2024-11-04 23:33:52 +0300

    working registration EP, users list(of dict), adding user_dict to users_list
    not finished, TODO:
    - validation of input registration form
    - signing EP
    - session!!!
    - other stuff

--- lesson07/1.py
+++ lesson07/1.py
@@ -31,18 +31,43 @@
 
 """
 
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, session, request
 from datetime import datetime
 import os
 import requests
 
 BASE_FOLDER = os.getcwd()
+clicks = 0
+users_list = []
+USER = {}
+login = False
 
 app = Flask(
     __name__,
     static_folder=os.path.join(BASE_FOLDER, "static"),
     template_folder=os.path.join(BASE_FOLDER, "templates"),
 )
+app.config["SECRET_KEY"] = "my_secret_key"
+
+
+def name_valid(name):
+    return True
+
+
+def login_valid(login):
+    return True
+
+
+def password_valid(password):
+    return True
+
+
+def email_valid(email):
+    return True
+
+
+def age_valid(age):
+    return True
 
 
 @app.route("/")
@@ -51,54 +76,78 @@ def index():
     return render_template("main.html", even_minutes_server=even_minutes_server)
 
 
+@app.route("/registration/", methods=["GET", "POST"])
+def registration():
+    if request.method == "GET":
+        return render_template("registration.html")
+    if request.method == "POST":
+        user_name = request.form.get("name")
+        user_login = request.form.get("login")
+        user_password = request.form.get("password")
+        user_email = request.form.get("email")
+        user_age = request.form.get("age")
+        if all(
+            [
+                name_valid(user_name),
+                login_valid(user_login),
+                password_valid(user_password),
+                email_valid(user_email),
+                age_valid(user_age),
+            ]
+        ):
+            if user_login not in [
+                awailable_user["login"] for awailable_user in users_list
+            ]:
+                users_list.append(
+                    {
+                        "name": user_name,
+                        "login": user_login,
+                        "password": user_password,
+                        "email": user_email,
+                        "age": user_age,
+                    }
+                )
+                print(users_list)
+                login = True
+                return redirect("/")
+            else:
+                pass
+
+
 @app.route("/duck/")
 def duck():
-    url = "https://random-d.uk/api/v2"
-    res = requests.get(f"{url}/random").json()
-    rand_duck_url = res["url"]
-    return render_template("duck.html", url=rand_duck_url)
+    if login:
+        url = "https://random-d.uk/api/v2"
+        res = requests.get(f"{url}/random").json()
+        rand_duck_url = res["url"]
+        return render_template("duck.html", url=rand_duck_url)
+    else:
+        return redirect("/registration/")
 
 
 @app.route("/fox/<int:count>/")
 def fox(count):
-    if 0 < count < 11:
-        foxes_urls_list = []
-        for _ in range(count):
-            url = "https://randomfox.ca/floof/"
-            res = requests.get(url).json()
-            rand_fox_url = res["image"]
-            foxes_urls_list.append(rand_fox_url)
-        return render_template("fox.html", urls=foxes_urls_list)
+    if login:
+        if 0 < count < 11:
+            foxes_urls_list = []
+            for _ in range(count):
+                url = "https://randomfox.ca/floof/"
+                res = requests.get(url).json()
+                rand_fox_url = res["image"]
+                foxes_urls_list.append(rand_fox_url)
+            return render_template("fox.html", urls=foxes_urls_list)
+        else:
+            return "<h1>указано неверное количество, укажите число от 1 до 10 включительно</h1>"
     else:
-        return "<h1>указано неверное количество, укажите число от 1 до 10 включительно</h1>"
+        return redirect("/registration/")
 
 
 @app.route("/weather-minsk/")
 def weather_minsk():
-    url = "https://api.openweathermap.org/data/2.5/weather"
-    params = {
-        "q": "Minsk",
-        "units": "metric",
-        "lang": "ru",
-        "APPID": "7505a3a45c2f09d2e4009d8887ceb17f",
-    }
-    res = requests.get(url, params).json()
-
-    return render_template(
-        "weather.html",
-        city="Minsk",
-        conditions=res["weather"][0]["description"],
-        temp=res["main"]["temp"],
-        feels_like=res["main"]["feels_like"],
-    )
-
-
-@app.route("/weather/<city>/")
-def weather_city(city):
-    try:
+    if login == True:
         url = "https://api.openweathermap.org/data/2.5/weather"
         params = {
-            "q": f"{city}",
+            "q": "Minsk",
             "units": "metric",
             "lang": "ru",
             "APPID": "7505a3a45c2f09d2e4009d8887ceb17f",
@@ -107,57 +156,92 @@ def weather_city(city):
 
         return render_template(
             "weather.html",
-            city=city.capitalize(),
+            city="Minsk",
             conditions=res["weather"][0]["description"],
             temp=res["main"]["temp"],
             feels_like=res["main"]["feels_like"],
         )
-    except:
-        return "<h1>введенный город не найден</h1>"
+    else:
+        return redirect("/registration/")
 
 
-@app.route("/weather-5city/")
-def weater_five():
-    even_minutes_server = datetime.now().minute % 2 == 0
-    if even_minutes_server:
-        weather_city_list = []
-        url = "https://api.openweathermap.org/data/2.5/weather"
-        city_list = ["Brest", "Vitebsk", "Gomel", "Grodno", "Mogilev"]
-        for _ in city_list:
+@app.route("/weather/<city>/")
+def weather_city(city):
+    if login == True:
+        try:
+            url = "https://api.openweathermap.org/data/2.5/weather"
             params = {
-                "q": _,
+                "q": f"{city}",
                 "units": "metric",
                 "lang": "ru",
                 "APPID": "7505a3a45c2f09d2e4009d8887ceb17f",
             }
             res = requests.get(url, params).json()
-            weather_city_list.append(
-                {
-                    "city": _,
-                    "conditions": res["weather"][0]["description"],
-                    "temp": res["main"]["temp"],
-                    "feels_like": res["main"]["feels_like"],
-                }
+
+            return render_template(
+                "weather.html",
+                city=city.capitalize(),
+                conditions=res["weather"][0]["description"],
+                temp=res["main"]["temp"],
+                feels_like=res["main"]["feels_like"],
             )
+        except:
+            return "<h1>введенный город не найден</h1>"
     else:
-        return redirect("/")
-    return render_template(
-        "weather-5city.html",
-        even_minutes_server=even_minutes_server,
-        weather_city_list=weather_city_list,
-    )
+        return redirect("/registration/")
+
+
+@app.route("/weather-5city/")
+def weater_five():
+    if login == True:
+        even_minutes_server = datetime.now().minute % 2 == 0
+        if even_minutes_server:
+            weather_city_list = []
+            url = "https://api.openweathermap.org/data/2.5/weather"
+            city_list = ["Brest", "Vitebsk", "Gomel", "Grodno", "Mogilev"]
+            for _ in city_list:
+                params = {
+                    "q": _,
+                    "units": "metric",
+                    "lang": "ru",
+                    "APPID": "7505a3a45c2f09d2e4009d8887ceb17f",
+                }
+                res = requests.get(url, params).json()
+                weather_city_list.append(
+                    {
+                        "city": _,
+                        "conditions": res["weather"][0]["description"],
+                        "temp": res["main"]["temp"],
+                        "feels_like": res["main"]["feels_like"],
+                    }
+                )
+        else:
+            return redirect("/")
+        return render_template(
+            "weather-5city.html",
+            even_minutes_server=even_minutes_server,
+            weather_city_list=weather_city_list,
+        )
+    else:
+        return redirect("/registration/")
 
 
 @app.route("/clicker/")
 def clicker():
-    return render_template("clicker.html", clicks=clicks)
+    if login == True:
+        return render_template("clicker.html", clicks=clicks)
+    else:
+        return redirect("/registration/")
 
 
 @app.route("/fake-clicker/")
 def fake_clicker():
-    global clicks
-    clicks += 1
-    return redirect("/clicker/")
+    if login == True:
+        global clicks
+        clicks += 1
+        return redirect("/clicker/")
+    else:
+        return redirect("/registration/")
 
 
 @app.errorhandler(404)
@@ -165,5 +249,4 @@ def page_not_found(error):
     return render_template("err.html")
 
 
-clicks = 0
 app.run(port=7777)
